4026891a3155ddc6a54a9d23d5d0382e84d539b5
Jeffrey Viveros <temel57@grupotemel.net>
2020-01-02 15:57:57 +0100

    Updates

---
+++ precache-manifest.c94a85a543523619e9ed44a87c0d7269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91db19343b7f6bcc8299e08945d0d530",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "93be59ad59835d2969b2",
+    "url": "/pomodoro-clock/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3d994a79e7ae77f68471",
+    "url": "/pomodoro-clock/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93be59ad59835d2969b2",
+    "url": "/pomodoro-clock/static/js/2.701160b0.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/pomodoro-clock/static/js/2.701160b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d994a79e7ae77f68471",
+    "url": "/pomodoro-clock/static/js/main.afde7140.chunk.js"
+  },
+  {
+    "revision": "5ec1cf88de947a217b79",
+    "url": "/pomodoro-clock/static/js/runtime-main.e4aa98e7.js"
+  }
+]);
